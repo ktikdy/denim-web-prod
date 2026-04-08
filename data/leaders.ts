af71392c370d49b98ba161c14966d4c8f9b1cd68
Gokberk Yar <gyar@denimhealth.com>
2026-04-08 20:53:01 +0000

    Remove Ben add Yar

--- data/leaders.ts
+++ data/leaders.ts
@@ -7,18 +7,18 @@ export const leaders = [
       "Mike Kadyan is a serial health tech founder dedicated to driving measurable results for hospitals and health systems. Mike is CEO and Co-founder of Denim Health and previously was CEO and Co-founder of Iodine Software, a healthcare SaaS unicorn, where he continues to serve as Chairman of the Board. Mike was also CEO and Co-founder of Crimson, a company aimed at enhancing healthcare provider quality, which was acquired by The Advisory Board Company. He holds a BS in Business Administration from Boston University and an MBA from Harvard Business School. Additionally, Mike serves as a Member of the Board of Trustees at Saint Stephen's Episcopal School.",
   },
   {
-    name: "Ben Gawiser",
-    title: "VP, Engineering",
-    image: "/images/team-member-3.png",
-    description:
-      "Ben Gawiser is a seasoned technology leader with over two decades of experience in software development, engineering management, and data science. As VP of Engineering and Chief Security and Compliance Officer at Denim Health, Ben drives innovation and ensures security across the company’s platforms. He previously held leadership roles at Amazon, The Advisory Board Company, RF Code, and Hive9, where he led product development, engineering processes, and cross-functional teams. Ben holds a BSE in Computer Science from Princeton University and an MBA with honors in Information Management from the University of Texas at Austin.",
-  },
-  {
     name: "Steffan Haithcox",
     title: "VP, Customer Engagement",
     image: "/images/team-member-4.png",
     description:
       "Steffan Haithcox has more than three decades of experience covering customer success, sales, marketing, and strategy - almost entirely in healthcare and health tech. He has a proven track record of driving growth in a variety of companies from VC-backed startups to mid-size PE portfolio companies to large, public enterprises. Steffan is currently VP of Customer Engagement at Denim Health and was previously Chief Marketing Officer at PerfectServe. He has also held leadership roles at bswift, Lawson, and AeroScout. Steffan has a BA from Swarthmore College and an MBA from Harvard Business School.",
+  },
+  {
+    name: "Gokberk Yar",
+    title: "Principal Engineer",
+    image: "/images/team-member-3.jpg",
+    description:
+      "Gokberk Yar is a technology leader specializing in artificial intelligence, machine learning, and scalable healthcare solutions. As Principal Software Engineer at Denim, he oversees the development of AI-powered platforms that automate healthcare operations and integrate with electronic health record systems. Gokberk is responsible for developing its core technology, cloud infrastructure, and production systems while supporting healthcare deployments. Prior to Denim, he conducted machine learning research at Northeastern University and published academic work on AI and distributed learning. He holds a BS in Computer Science from Sabanci University, graduating first in his class, and an MS in Computer Science from Northeastern University."
   },
   {
     name: "Satya Gunnam",
